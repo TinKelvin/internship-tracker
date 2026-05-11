d5297fdcfeba577be19f121c1e31f45e96a48c93
Tin Seung Yuen Kelvin <RubyTGM@users.noreply.github.com>
2026-05-11 19:24:59 -0400

    Completed full CRUD API for internship tracker

--- src/main/java/com/KTSY/InternshipTracker/controller/ApplicationController.java
+++ src/main/java/com/KTSY/InternshipTracker/controller/ApplicationController.java
@@ -2,8 +2,11 @@ package com.KTSY.InternshipTracker.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,34 @@ public class ApplicationController {
     @PostMapping
     public Application createApplication(@RequestBody Application application) {
     return repository.save(application);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteApplication(@PathVariable Long id) {
+    repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Application updateApplication(
+            @PathVariable Long id,
+            @RequestBody Application updatedApplication) {
+
+        Application application = repository.findById(id)
+                .orElseThrow();
+
+        application.setCompany(updatedApplication.getCompany());
+        application.setRole(updatedApplication.getRole());
+        application.setStatus(updatedApplication.getStatus());
+        application.setLocation(updatedApplication.getLocation());
+        application.setDateApplied(updatedApplication.getDateApplied());
+
+        return repository.save(application);
+    }
+
+    @GetMapping("/{id}")
+    public Application getApplicationById(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow();
     }
 
 }
